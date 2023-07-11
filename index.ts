a15b4a9a80fbfb9f00ac63ed84ad65a6cc081f5d
nikita-remeslov <nikita.remeslov@binary-studio.com>
2023-07-11 11:20:20 +0200

    - webpack; + vite

--- index.ts
+++
@@ -1,6 +0,0 @@
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './src/styles/styles.css';
-import { render } from './src/index';
-
-render();
---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/styles.css';
+
+// TODO render your app here
